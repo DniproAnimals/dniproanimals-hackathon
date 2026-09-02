20591cad4543881c4394277528e83feadeda93c4
Maksym Hopaitsa <77167962+Aqu1ver1@users.noreply.github.com>
2026-09-02 21:22:40 +0200

    contacts added to bd and created dashboard feat for editing (#51)

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -4,4 +4,5 @@ import { getDatabaseUrl } from "./utils/get-database-url";
 
 export const db = drizzle(getDatabaseUrl());
 export * from "drizzle-orm";
+export * from "./db/defaults";
 export * from "./db/schema";
