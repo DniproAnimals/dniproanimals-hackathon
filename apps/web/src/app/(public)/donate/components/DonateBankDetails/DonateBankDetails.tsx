4e85fff0429c0a50c57432fae984643caf99703e
Aqu1ver1 <maksim260107@gmail.com>
2026-09-06 12:59:06 +0200

    readded bankdetailsFile

---
+++ apps/web/src/app/(public)/donate/components/DonateBankDetails/DonateBankDetails.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { IconCreditCard } from "@dniproanimals/icons";
+import { Card } from "@dniproanimals/ui";
+
+export function DonateBankDetails() {
+  // const { data: bankDetails } = useBankDetailsQuery();
+
+  return (
+    <Card className="p-8 rounded-3xl border-gray-100 shadow-sm mb-8">
+      <h2 className="text-xl font-bold mb-6 flex items-center gap-3">
+        <IconCreditCard size={24} />
+        Прямі банківські реквізити
+      </h2>
+    </Card>
+  );
+}
