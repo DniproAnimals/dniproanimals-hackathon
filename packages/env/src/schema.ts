37fc74d66d68d50e30190137f530fca031c8ce2d
unblvvv <nekitcom111@gmail.com>
2026-09-10 18:26:58 +0300

    chore: update env

--- packages/env/src/schema.ts
+++ packages/env/src/schema.ts
@@ -5,12 +5,14 @@ export const envSchema = z.object({
     .enum(["development", "production", "test"])
     .default("development"),
 
-  DATABASE_URL: z.string().optional(),
-  DB_HOST: z.string().default("localhost"),
-  DB_PORT: z.coerce.number().default(55432),
-  DB_USER: z.string().default("postgres"),
-  DB_PASS: z.string().default("postgres"),
-  DB_NAME: z.string().default("dniproanimals"),
+  DATABASE_URL: z.string().min(1).optional(),
+  DATABASE_URL_UNPOOLED: z.string().min(1).optional(),
+  NEON_BRANCH: z.string().optional(),
+  // DB_HOST: z.string().default("localhost"),
+  // DB_PORT: z.coerce.number().default(5432),
+  // DB_USER: z.string().default("postgres"),
+  // DB_PASS: z.string().default("postgres"),
+  // DB_NAME: z.string().default("dniproanimals"),
 
   REDIS_HOST: z.string().default("localhost"),
   REDIS_PORT: z.coerce.number().default(6379),
